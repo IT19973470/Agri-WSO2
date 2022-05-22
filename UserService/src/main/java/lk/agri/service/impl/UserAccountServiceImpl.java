4a91712da72e13dfa51f7def4fa0300a0a8eb872
imalka <imalka@gmail.com>
2022-05-23 01:06:10 +0530

    Update UserAccountServiceImpl.java

--- UserService/src/main/java/lk/agri/service/impl/UserAccountServiceImpl.java
+++ UserService/src/main/java/lk/agri/service/impl/UserAccountServiceImpl.java
@@ -4,6 +4,7 @@ import lk.agri.dto.UserAccountDTO;
 import lk.agri.entity.UserAccount;
 import lk.agri.repository.UserAccountRepository;
 import lk.agri.service.UserAccountService;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,12 +19,16 @@ public class UserAccountServiceImpl implements UserAccountService {
 
     @Override
     public UserAccountDTO login(UserAccount userAccount) {
+        byte[] encodedBytes = Base64.encodeBase64(userAccount.getPassword().getBytes());
+        userAccount.setPassword(new String(encodedBytes));
         UserAccount userAccountObj = userAccountRepository.findByEmailAndPassword(userAccount.getEmail(), userAccount.getPassword());
         return new UserAccountDTO(userAccountObj);
     }
 
     @Override
     public UserAccountDTO signUp(UserAccount userAccount) {
+        byte[] encodedBytes = Base64.encodeBase64(userAccount.getPassword().getBytes());
+        userAccount.setPassword(new String(encodedBytes));
         UserAccountDTO userAccountDTO = new UserAccountDTO(userAccountRepository.save(userAccount));
         userAccountDTO.setPassword(null);
         return userAccountDTO;
