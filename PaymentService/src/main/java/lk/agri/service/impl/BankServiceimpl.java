c15aea389644e0a7c25d5a481014b28bbcfc557b
imalka <imalka@gmail.com>
2022-05-22 13:59:42 +0530

    email

--- PaymentService/src/main/java/lk/agri/service/impl/BankServiceimpl.java
+++ PaymentService/src/main/java/lk/agri/service/impl/BankServiceimpl.java
@@ -1,9 +1,9 @@
 package lk.agri.service.impl;
 
-import lk.agri.dto.PurchaseDTO;
 import lk.agri.entity.Purchase;
 import lk.agri.repository.PurchaseRepository;
 import lk.agri.service.BankService;
+import lk.agri.service.SendEmailSMTP;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +14,21 @@ import java.time.format.DateTimeFormatter;
 public class BankServiceimpl implements BankService {
     @Autowired
     private PurchaseRepository purchaseRepository;
+    @Autowired
+    private SendEmailSMTP sendEmailSMTP;
 
     @Override
     public Purchase addPayment(Purchase purchase) {
         String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddhhmmss"));
         purchase.setTransactionID("T" + dateTime);
-        purchase = purchaseRepository.save(purchase);
+        Purchase purchaseObj = purchaseRepository.save(purchase);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                sendEmailSMTP.sendEmail(purchaseObj.getEmail(), "Payment Success",
+                        "You are successfully paid for the cart. Your payment ID is " + purchase.getTransactionID());
+            }
+        }).start();
         return purchase;
 
     }
